02f2c3a16d56550e28d9558fbe135a35775ccb19
mohadeseh <m.rezvan1998@gmail.com>
2025-08-20 23:40:03 +0330

    add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { Button } from "flowbite-react";
+import Navbar from "./Components/Navbar";
 function App() {
-
   return (
     <>
-    <h1 className="text-3xl font-bold underline">hello word</h1>
-    <div className="p-4">
-      <Button>Ajab</Button>
-    </div>
-     </>
+    <Navbar/>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Navbar} from "flowbite-react"
+export default function myNavbar() {
+    return (
+        <Navbar className="flex justify-between items-center bg-white border-gray-200  gap-x-6">
+       <div className="flex items-center px-32">
+         <a href="#">
+          <img src="./src/assets/icon (1).svg" className="" alt="" />
+         </a>
+        </div>
+        <ul className="list-none flex items-center justify-between gap-x-6 px-36">
+          <li>
+            <a href="#" className="font-HORIZONTAL font-bold py-2 px-3 text-[#A8AEBF] hover:text-purple-500">Movies</a>
+          </li>
+          <li>
+            <a href="#" className="font-HORIZONTAL font-bold py-2 px-3 text-[#A8AEBF] hover:text-purple-500">TV Shows</a>
+          </li>
+          <li>
+            <a href="#" className="font-HORIZONTAL font-bold py-2 px-3 text-[#A8AEBF] hover:text-purple-500">Suggest me</a>
+          </li>
+        </ul>
+    </Navbar>
+    );
+}
