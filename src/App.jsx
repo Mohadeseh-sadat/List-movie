b67e4601a19c86a446be7367c712a3e5113be622
mohadeseh <m.rezvan1998@gmail.com>
2025-08-20 10:21:09 +0330

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "flowbite-react";
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">hello word</h1>
+    <div className="p-4">
+      <Button>Ajab</Button>
+    </div>
+     </>
+  );
+}
+
+export default App
