352d8f9cba706530a3662bc313b4619b1273c0b0
mohadeseh <m.rezvan1998@gmail.com>
2025-08-31 00:15:14 +0330

    design detailspage

--- src/Components/DetailsPage.jsx
+++ src/Components/DetailsPage.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Card } from "flowbite-react";
+
 
 export default function DetailsPage() {
   const { id } = useParams();
@@ -27,17 +29,29 @@ export default function DetailsPage() {
   if (!movie) return <p className="text-center text-white mt-6">فیلم پیدا نشد!</p>;
 
   return (
-    <div className="flex flex-col gap-6 p-6">
+    <div className="relativ h-full gap-6 p-6">
       <img
         src={movie.poster}
         alt={movie.title}
-        className="w-[1200px] h-[480px] mt-8 m-auto rounded-lg shadow-lg"
+        className="w-[1200px] h-[480px] mt-8 m-auto rounded-[40px] shadow-lg"
       />
-      <h1 className=" text-gray-300 text-3xl font-bold mt-6">{movie.title}</h1>
-      <p className="mt-2 text-gray-300">{movie.description}</p>
-      <p className="mt-2">سال ساخت: {movie.year}</p>
-      <p>امتیاز: {movie.rating}</p>
-      <p>ژانرها: {movie.genres?.join(" / ")}</p>
-    </div>
+      <div className="absolute w-[560px] h-[144px] bg-gray-800/80 backdrop-blur-sm rounded-3xl from-black/80 bottom-1/30 left-1/6 mb-[70px]">
+      <h1 className="mt-[36px] ml-10 text-white font-Poppins text-[32px] text-3xl font-bold">{movie.title}</h1></div>
+      
+      
+        <div className="flex flex-row gap-10 ">
+          <img  className="w-[480px] h-[720px] ml-[200px] mt-28 rounded-3xl"src={movie.poster} alt="" /> 
+         <div className="mt-28 gap-10" vertical>
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Part of the journey is the end.
+            </h3>
+            <p className="mt-2 text-gray-300">{movie.description}</p>
+            <p className="mt-2 text-white">سال ساخت: {movie.year}</p>
+            <p className="text-white">امتیاز: {movie.rating}</p>
+            <p className="text-white">ژانرها: {movie.genres?.join(" / ")}</p>
+         </div>
+        </div>
+      </div>
+  
   );
 }
